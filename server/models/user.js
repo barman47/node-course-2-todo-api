3c951d3e5f89c11514e28e123892f22556739d12
barman47 <nomsouzoanya@yahoo.co.uk>
2018-04-17 01:39:25 +0100

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
+    }
+});
